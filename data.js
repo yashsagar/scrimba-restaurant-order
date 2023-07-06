b50d687573da5b7e7c2810b36f9c73896e31f076
yashsagar <sagar.sagar75@gmail.com>
2023-07-06 19:28:17 +0530

    image error fix

--- data.js
+++ data.js
@@ -1,29 +1,29 @@
 export const menuArray = [
-    {
-        name: "Pizza",
-        ingredients: ["pepperoni", "mushrom", "mozarella"],
-        id: 0,
-        price: 14,
-        emoji: "🍕",
-        icon:"/images/pizza-icon.png",
-        alt:"image of pizza slice"
-    },
-    {
-        name: "Hamburger",
-        ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
-        emoji: "🍔",
-        id: 1,
-        icon:"/images/burger.png",
-        alt:"image of burger"
-    },
-        {
-        name: "Beer",
-        ingredients: ["grain, hops, yeast, water"],
-        price: 12,
-        emoji: "🍺",
-        id: 2,
-        icon:"/images/beer.jpg",
-        alt:"image of beer"
-    }
-]
+  {
+    name: "Pizza",
+    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    id: 0,
+    price: 14,
+    emoji: "🍕",
+    icon: "./images/pizza-icon.png",
+    alt: "image of pizza slice",
+  },
+  {
+    name: "Hamburger",
+    ingredients: ["beef", "cheese", "lettuce"],
+    price: 12,
+    emoji: "🍔",
+    id: 1,
+    icon: "./images/burger.png",
+    alt: "image of burger",
+  },
+  {
+    name: "Beer",
+    ingredients: ["grain, hops, yeast, water"],
+    price: 12,
+    emoji: "🍺",
+    id: 2,
+    icon: "./images/beer.jpg",
+    alt: "image of beer",
+  },
+];
